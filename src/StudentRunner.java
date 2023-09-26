b488c910ee09443cf61ec3bb40c1b46c7d8e50ce
Steven Zheng <stevenz85@nycstudents.net>
2023-09-25 21:35:21 -0400

    The start of my code

--- src/StudentRunner.java
+++
@@ -1,7 +0,0 @@
-public class StudentRunner {
-    public static void main(String[] args) {
-        Student student1 = new Student("Jarvis", 16, "Math");
-        student1.introduce();
-        student1.school();
-    }
-}
