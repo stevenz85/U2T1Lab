7893704b308097cb22beadcf2232934154003ecf
Steven Zheng <stevenz85@nycstudents.net>
2023-09-25 21:36:43 -0400

    I have completed my StudentRunner.java

---
+++ src/StudentRunner.java
@@ -0,0 +1,7 @@
+public class StudentRunner {
+    public static void main(String[] args) {
+        Student student1 = new Student("Jarvis", 16, "Math");
+        student1.introduce();
+        student1.school();
+    }
+}
